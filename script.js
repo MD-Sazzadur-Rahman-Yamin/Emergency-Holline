afaa6468e26e4184a3189559f5dad2ba61a07c81
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-08-29 22:48:06 +0600

    Added functionality to copy button

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ for (const heart of hearts) {
   });
 }
 
-//
+//copy Button Function
+const copys = document.querySelectorAll(".btn-1");
+const copyCount = document.getElementById("copy-count");
+let copyinitialCount = 0;
+for (const copy of copys) {
+  copy.addEventListener("click", function () {
+    copyinitialCount++;
+    copyCount.innerText = copyinitialCount;
+    // Copy the number
+      const copyHotlineNumber = copy.parentNode.parentNode.children[2].children[0].innerText;
+      navigator.clipboard.writeText(copyHotlineNumber);
+      alert(copyHotlineNumber + " copied");
+      console.log(copyHotlineNumber);
+  });
+
+}
+
+// Clear history function
 document.getElementById("clear-btn").addEventListener("click", function () {
   document.getElementById("history-list-container").innerText = "";
 });
